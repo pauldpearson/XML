edd5c85a480936264bee51bb09f852659237f20d
Paul Pearson <pauldarlingtonpearson@gmail.com>
2017-09-27 19:20:58 -0600

    HW3 revision, added Quiz
    Signed-off-by: Paul Pearson <pauldarlingtonpearson@gmail.com>

--- XML_Data_DU/HWStarters/convert.py
+++ XML_Data_DU/HWStarters/convert.py
@@ -1,3 +1,12 @@
+import xml.etree.ElementTree as ET
 
+president_data = ET.parse('USPresidents.xml')
+birthday_data = ET.parse('PresidentBirthdays.xml')
 
+root_president = president_data.getroot()
+root_birthday = birthday_data.getroot()
 
+# print ET.tostring(root_president)
+# print ET.tostring(root_birthday)
+
+print root_birthday.iterfind('birthday')
